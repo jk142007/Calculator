9a36bf5a44c828e23a28f70a2b71b08df4b4a5a7
jk142007 <jambukiyakhushali@gmail.com>
2025-06-18 10:10:06 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -29,5 +29,5 @@ def calculator():
     return render_template('calc.html', result=result)
 
 if __name__ == '__main__':
- port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
