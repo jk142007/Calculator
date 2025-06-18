186071d4c22afb6a1846ae9cdcfbb3c4fc1b5c52
jk142007 <jambukiyakhushali@gmail.com>
2025-06-18 09:56:27 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    result = ""
+    if request.method == 'POST':
+        try:
+            num1 = float(request.form['num1'])
+            num2 = float(request.form['num2'])
+            operation = request.form['operation']
+            
+            if operation == 'add':
+                result = num1 + num2
+            elif operation == 'subtract':
+                result = num1 - num2
+            elif operation == 'multiply':
+                result = num1 * num2
+            elif operation == 'divide':
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    result = "Cannot divide by zero"
+            else:
+                result = "Invalid operation"
+        except:
+            result = "Please enter valid numbers"
+    return render_template('calc.html', result=result)
+
+if __name__ == '__main__':
+ port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
